783ef9500378997edbe5c6c464177320eb5561d5
NotebookGidora <artemjay@yandex.ru>
2023-05-14 03:13:17 +0300

    Refactor: Исправить названия перменных и вынести массив карточек в модуль

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Mountain',
+        link: 'https://images.unsplash.com/photo-1683520596266-b7811d63e5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    },
+    {
+        name: 'Desert',
+        link: 'https://images.unsplash.com/photo-1683526976156-1a3f1a315049?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+    },
+    {
+        name: 'Soldiers are resting on the field',
+        link: 'https://images.unsplash.com/photo-1683480678676-9aff6d4cf61c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1118&q=80',
+    },
+    {
+        name: 'Zürich, Switzerland',
+        link: 'https://images.unsplash.com/photo-1683377117046-99ca0f5a8291?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    },
+    {
+        name: 'Crocodiles',
+        link: 'https://images.unsplash.com/photo-1683640528593-f3e19d9e06ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    },
+    {
+        name: 'Osaka, Japan',
+        link: 'https://images.unsplash.com/photo-1683459597762-0d8cf92b6c1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    },
+]
--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,26 @@
-let userName = document.querySelector('.profile__title'),
+const userName = document.querySelector('.profile__title'),
     userDescription = document.querySelector('.profile__text'),
     buttonProfileEdit = document.querySelector('.profile__button_type_edit'),
     popupEditProfile = document.querySelector('.popup_type_edit'),
     buttonCloseEditPopup = document.querySelector('#CloseEdit'),
     userNameInput = document.querySelector('.popup__input_type_name'),
     userDescriptionInput = document.querySelector('.popup__input_type_description'),
-    editFormSubmit = document.querySelector('#popup_edit_submit');
+    formEditSubmit = document.querySelector('#popup_edit_submit');
 
-let buttonPlaceAdd = document.querySelector('.profile__button_type_add'),
+const buttonPlaceAdd = document.querySelector('.profile__button_type_add'),
     buttonCloseAddPopup = document.querySelector('#CloseAdd'),
     popupAddPlace = document.querySelector('.popup_type_add'),
     placeNameInput = document.querySelector('.popup__input_type_place'),
     placeLinkInput = document.querySelector('.popup__input_type_link'),
     placeAddFormSubmit = document.querySelector('#popup_add_submit');
 
-let buttonCloseImage = document.querySelector('#CloseImage'),
+const buttonCloseImage = document.querySelector('#CloseImage'),
     popupTypeImage = document.querySelector('.popup_type_image'),
     imagePopup = document.querySelector('.popup__image'),
     figcaption = document.querySelector('.popup__figcaption');
 
-let template = document.querySelector('#template_card').content;
-let cardsSection = document.querySelector('.cards');
-const initialCards = [
-    {
-        name: 'Mountain',
-        link: 'https://images.unsplash.com/photo-1683520596266-b7811d63e5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    },
-    {
-        name: 'Desert',
-        link: 'https://images.unsplash.com/photo-1683526976156-1a3f1a315049?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
-    },
-    {
-        name: 'Soldiers are resting on the field',
-        link: 'https://images.unsplash.com/photo-1683480678676-9aff6d4cf61c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1118&q=80',
-    },
-    {
-        name: 'Zürich, Switzerland',
-        link: 'https://images.unsplash.com/photo-1683377117046-99ca0f5a8291?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    },
-    {
-        name: 'Crocodiles',
-        link: 'https://images.unsplash.com/photo-1683640528593-f3e19d9e06ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    },
-    {
-        name: 'Osaka, Japan',
-        link: 'https://images.unsplash.com/photo-1683459597762-0d8cf92b6c1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    },
-]
+const template = document.querySelector('#template_card').content;
+const cardsSection = document.querySelector('.cards');
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
@@ -59,6 +33,7 @@ function openPopup(popup) {
 function renderCard(cardItem) {
     let cardElement = template.querySelector('.cards__item').cloneNode(true);
     cardElement.querySelector('.cards__img').src = cardItem.link;
+    cardElement.querySelector('.cards__img').alt = cardItem.name;
     cardElement.querySelector('.cards__title').textContent = cardItem.name;
     const like = cardElement.querySelector('.cards__like');
     like.addEventListener('click', () => {
@@ -69,13 +44,14 @@ function renderCard(cardItem) {
     cardElement.querySelector('.cards__img')
         .addEventListener('click', () => {
             imagePopup.src = cardItem.link;
+            imagePopup.alt = cardItem.name;
             figcaption.textContent = cardItem.name;
             openPopup(popupTypeImage);
         })
     cardsSection.prepend(cardElement);
 }
 
-initialCards.map(renderCard)
+initialCards.forEach(renderCard)
 
 buttonProfileEdit.addEventListener('click', () => {
     openPopup(popupEditProfile)
@@ -88,7 +64,7 @@ buttonCloseEditPopup.addEventListener('click', () => closePopup(popupEditProfile
 buttonCloseAddPopup.addEventListener('click', () => closePopup(popupAddPlace));
 buttonCloseImage.addEventListener('click', () => closePopup(popupTypeImage))
 
-editFormSubmit.addEventListener('submit', (event) => {
+formEditSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
     userName.textContent = `${userNameInput.value}`;
     userDescription.textContent = `${userDescriptionInput.value}`;
