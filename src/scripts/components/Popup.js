2b67dd1ecb91a266cd9f480400f2ed63e4bcfb09
Artem <artemjay@yandex.ru>
2023-06-11 15:47:56 +0300

    Feat: Popup class

---
+++ src/scripts/components/Popup.js
@@ -0,0 +1,34 @@
+export default class Popup {
+    constructor(selector) {
+        this._popup = document.querySelector(selector);
+        this._closeButton = this._popup.querySelector('.popup__close');
+        this._escClose = this._handleEscClose.bind(this);
+    }
+
+    open() {
+        this._popup.classList.add('popup_opened');
+        document.addEventListener('keydown', this._escClose);
+    }
+
+    close() {
+        this._popup.classList.remove('popup_opened');
+        document.removeEventListener('keydown', this._escClose)
+    }
+
+    _handleEscClose(event) {
+        if (event.key === 'Escape') {
+            this._popup.close();
+        }
+    }
+
+    setEventListeners() {
+        this._closeButton.addEventListener('click', () => {
+            this.close();
+        })
+        this._popup.addEventListener('click', (event) => {
+            if (event.target === event.currentTarget) {
+                this.close();
+            }
+        })
+    }
+}
